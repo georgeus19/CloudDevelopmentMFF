37c3be08db0f11f74a09b399c1319aa824db89fe
krystof <hrubyk97@gmail,com>
2022-03-22 15:33:03 +0100

    managed identity

---
+++ lab6/MyWebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApplication.Pages
+{
+	public class IndexModel : PageModel
+	{
+
+		public List<BlobItem>? Blobs { get; set; }
+
+		public void OnGet()
+		{
+			//Blobs = new List<BlobItem>();
+            Blobs = CreateClient().GetBlobs().ToList();
+        }
+
+		public FileStreamResult OnGetDownloadBlob(string blobName)
+		{
+			var blobClient = CreateClient().GetBlobClient(blobName);
+			var contentType = blobClient.GetProperties().Value.ContentType;
+			var stream = blobClient.OpenRead();
+			return new FileStreamResult(stream, contentType);
+		}
+
+		private BlobContainerClient CreateClient()
+		{
+			Uri containerEndpoint = new Uri("https://cloudmffslab3storageacc.blob.core.windows.net/lab3container");
+			return new Azure.Storage.Blobs.BlobContainerClient(containerEndpoint, new Azure.Identity.DefaultAzureCredential());
+		}
+	}
+}
